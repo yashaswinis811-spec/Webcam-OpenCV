e76fffef65477f1941b3568d91c39ec8d17bacf6
yashaswinis811-spec <yashaswinis811@gmail.com>
2026-08-18 15:15:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import module1
+import module2
+
+a = 20
+b = 5
+
+print("Addition:", module1.addition(a, b))
+print("Subtraction:", module1.subtraction(a, b))
+print("Multiplication:", module2.multiplication(a, b))
+print("Division:", module2.division(a, b))
